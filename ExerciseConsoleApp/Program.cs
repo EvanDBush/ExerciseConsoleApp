20e91344f11cef287eaf145fbb697191176f1e16
Evan Bush <e.bush05@gmail.com>
2022-03-15 13:51:36 -0400

    first commit

---
+++ ExerciseConsoleApp/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace ExerciseConsoleApp 
+{
+    class Program 
+    {
+    static void Main(string[] args)
+        {
+            Console.WriteLine("Hello! Did you work out today?");
+            Console.WriteLine("Type 1 for Yes");
+            Console.WriteLine("Type 2 for No");
+
+            string userOpeningResponse = Console.ReadLine();
+
+            if (userOpeningResponse = "1")
+// Set up Menu
+            }
+
+            if (userOpeningResponse = "2")
+            {
+// get current time. calculate how much time is left in a day.
+            }
+            else
+            {
+                Console.WriteLine("I'm sorry. Please try entering a 1 or 2 again.");
+            }
+        }
+    }
+
+
+
+}
